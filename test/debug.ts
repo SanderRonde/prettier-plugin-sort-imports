fed3dc375b9d436e13c37a4f299add01af693d47
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-01-12 17:42:50 +0100

    add debugger file

---
+++ test/debug.ts
@@ -0,0 +1,42 @@
+import { PrettierOptions, SORTING_TYPE } from '../app/types';
+import { ParserOptions } from 'prettier';
+import * as fs from 'fs/promises';
+
+const app = require('../app/index') as {
+	parsers: {
+		typescript: {
+			preprocess: (text: string, options: any) => string;
+		};
+	};
+};
+const transform = app.parsers.typescript.preprocess;
+
+async function main() {
+	if (process.argv.length < 3) {
+		console.error('Missing file to debug');
+		process.exit(1);
+	}
+
+	const options: Omit<PrettierOptions, keyof ParserOptions> = {
+		sortingMethod: SORTING_TYPE.LINE_LENGTH,
+		stripNewlines: false,
+	};
+
+	if (process.argv.includes('--sorting=alphabetical')) {
+		options.sortingMethod = SORTING_TYPE.ALPHABETICAL;
+	}
+	if (process.argv.includes('--strip-newlines')) {
+		options.stripNewlines = true;
+	}
+
+	console.log(
+		transform(
+			await fs.readFile(process.argv[2], { encoding: 'utf8' }),
+			options
+		)
+	);
+}
+
+(async () => {
+	await main();
+})();
